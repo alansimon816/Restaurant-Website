8ac54e61b094ada004a9a563d446ca0a5ccc926c
Alan <alansimon816@gmail.com>
2021-05-23 18:27:35 -0400

    Creates modules for home page and header tabs.

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import { createHeader } from './tabs'
+
+const loadHomePage = () => {
+    createHeader()
+    let div = document.querySelector('#content')
+    let h1 = document.createElement('h1')
+    let h2 = document.createElement('h2')
+    h1.innerHTML = "Nouvo Vesuvio"
+    div.appendChild(h1)
+    h2.innerHTML = "Italian delicacies served to you on a silver platter."
+    div.appendChild(h2)
+}
+
+export {loadHomePage};
---
+++ src/tabs.js
@@ -0,0 +1,36 @@
+const createHeader = () => {
+    let header = document.createElement('header')
+    let home = document.createElement('div')
+    let menu = document.createElement('div')
+    let order = document.createElement('div')
+    let contact = document.createElement('div')
+    let tabs = [home, menu, order, contact]
+
+    home.innerHTML = "Home"
+    menu.innerHTML = "Menu"
+    order.innerHTML = "Order"
+    contact.innerHTML = "Contact"
+
+    tabs.forEach(elem => {
+        elem.setAttribute('ID', `${elem.innerHTML}`)
+        elem.setAttribute('class', 'tabs')
+        elem.addEventListener('mouseover', illuminate)
+        elem.addEventListener('mouseout', dim)
+    })
+
+    header.append(home, menu, order, contact)
+    document.querySelector('#content').appendChild(header)
+}
+
+function illuminate(e) {
+    let target = e.target;
+    target.style.textShadow = 
+    '1px 1px 2px black, 0 0 25px yellow, 0 0 5px goldenrod'
+}
+
+function dim(e) {
+    let target = e.target;
+    target.style.removeProperty('text-shadow') 
+}
+
+export { createHeader }
