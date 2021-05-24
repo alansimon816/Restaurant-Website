54c89519eee3236af67fc8897b75df5f67cc2087
Alan <alansimon816@gmail.com>
2021-05-24 13:38:45 -0400

    Add reviews to the home page.

--- src/home.js
+++ src/home.js
@@ -25,6 +25,32 @@ const createImage = () => {
     document.querySelector('#content').appendChild(div)
 }
 
+const createReviews = () => {
+    let reviews = document.createElement('div')
+    let review1 = document.createElement('p')
+    let review2 = document.createElement('p')
+
+    reviews.id = 'reviews'
+    review1.id = 'review1'
+    review2.id = 'review2'
+    review1.innerHTML = 
+    `★	★	★	★	★	<br>
+    Vesuvio's blew me away with their rich, layered lasagna. I've never had
+    anything like it! I highly recommend this restaurant to anyone that wants
+    the finest Italian dining experience possible on the East Coast!<br>
+    - Emily Bentega via Yelp, 2021`
+    review2.innerHTML =
+    `★	★	★	★	★<br>
+    I love Vesuvio's sliced gabagool as an appetizer. It always tastes fresh
+    and my family loves it! It pairs very well with the large selection of 
+    fancy wines they have available. Oh, and do yourself a favor and try their
+    Rosemary Mushroom Risotto... It's to die for!<br>
+                                            - Ken M via Google Reviews, 2021`
+    reviews.append(review1, review2)
+    document.querySelector('#content').appendChild(reviews) 
+
+}
+
 const createHomePage = () => { 
     createHeader()
     createImage()
@@ -36,6 +62,7 @@ const createHomePage = () => {
     h2.innerHTML = "Italian delicacies served to you on a silver platter."
     div.appendChild(h2)
     createDescription()
+    createReviews()
     createFooter() 
 }
 
